c155664cc35bf8b8682841991c1c665a5a2562db
RozovaOlga <olinkakrasavitsa@gmail.com>
2020-02-16 01:15:48 +0200

    15.02

--- src/Number7.java
+++ src/Number7.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.Callable;
+
 /**
  * Имеются 2 дома размерами  a  на b и с на d.Имеется участо размерами e на f.
  * Проверить, помещаются ли эти дома на участке.
@@ -6,19 +8,75 @@
 public class Number7 {
 
     public static void main(String[] args) {
-        int house1Length = 4;
-        int house1Width = 3;
-        int house2Length = 3;
-        int house2Width = 1;
-        int landLength = 4;
-        int landWidth = 10;
-        if (((house1Length + house2Length) <= landLength) && ((house1Width + house2Width) <= landWidth)) {
+        Home home1 = new Home();
+        home1.setLength(4);
+        home1.getLength();
+        home1.setWidth(3);
+        home1.getWidth();
+        Home home2 = new Home();
+        home2.setLength(1);
+        home2.getLength();
+        home2.setWidth(2);
+        home2.getLength();
+        Land land = new Land();
+        land.setLength(5);
+        land.setWidth(9);
+        Calculate calculate = new Calculate();
+        calculate.testCalculate(home1, home2, land);
+    }
+}
+
+class Home {
+    int length;
+    int Width;
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setWidth(int width) {
+        this.Width = width;
+    }
+
+    public int getWidth() {
+        return Width;
+    }
+}
+
+class Land {
+    int length;
+    int Width;
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setWidth(int width) {
+        this.Width = width;
+    }
+
+    public int getWidth() {
+        return Width;
+    }
+}
+
+class Calculate {
+    public void testCalculate(Home home1, Home home2, Land land) {
+        if (((home1.length + home2.length) <= land.length) && ((home1.Width + home2.Width) <= land.Width)) {
             System.out.println("Помещаются");
-        } else if (((house1Length + house2Width) <= landLength) && ((house1Width + house2Length) <= landWidth)) {
+        } else if (((home1.length + home2.Width) <= land.length) && ((home1.Width + home2.length) <= land.Width)) {
             System.out.println("Помещаются");
-        } else if (((house1Length + house2Width) <= landWidth) && ((house1Width + house2Length) <= landLength)) {
+        } else if (((home1.length + home2.Width) <= land.Width) && ((home1.Width + home2.length) <= land.length)) {
             System.out.println("Помещаются");
-        } else if (((house1Length + house2Length) <= landWidth) && ((house1Width + house2Width) <= landLength)) {
+        } else if (((home1.length + home2.length) <= land.Width) && ((home1.Width + home2.Width) <= land.length)) {
             System.out.println("Помещаются");
         } else {
             System.out.println("Не помещаются");
