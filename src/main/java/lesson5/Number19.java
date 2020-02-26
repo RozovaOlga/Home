9e91cfd829349167ce3129cc573041443060cc9f
RozovaOlga <olinkakrasavitsa@gmail.com>
2020-02-26 17:14:21 +0200

    изменила текст

--- src/main/java/lesson5/Number19.java
+++ src/main/java/lesson5/Number19.java
@@ -13,9 +13,8 @@ public class Number19 {
      * @param args the input arguments
      */
     public static void main(String[] args) {
-        String text = "   \uD83C\uDF89    \uD83D\uDE04 \uD83D\uDC4F    \uD83C\uDF89    ";
+        String text = "  Скучный и обыденный     текст     ";
         int number = text.trim().split(" \\s*").length;
         System.out.println(number);
-        System.out.println(text);
     }
 }
