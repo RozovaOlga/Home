972b3669913a5f91b20c9bf3360c5df9de2c12dc
RozovaOlga <olinkakrasavitsa@gmail.com>
2020-03-01 00:14:49 +0200

    добавила 25 задачу

--- src/main/java/lesson6/number24/ProgramLaunch.java
+++ src/main/java/lesson6/number24/ProgramLaunch.java
@@ -1,6 +1,11 @@
 package main.java.lesson6.number24;
 
-public class Work {
+/**
+ * Создать иерархию классов,описывающих бытовую технику.
+ * Создать несколько объектов описанных классов,часть их них включить в розетку.
+ * Иерархия должна иметь хотя бы три уровня
+ */
+public class ProgramLaunch {
     public static void main(String[] args) {
         Appliances appliances = new Appliances("Sss", 12);
         Washing washing = new Washing("Samsung", 5, 10, 15, 22, 44, 1, 12);
