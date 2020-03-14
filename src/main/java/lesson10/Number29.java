edd4a9beaa8169189c64746a115fd9f6edd9a7d1
RozovaOlga <olinkakrasavitsa@gmail.com>
2020-03-14 14:38:54 +0200

    29 разбила на 2 класса - оснвное решение и альтернативные

--- src/main/java/lesson10/Number29.java
+++ src/main/java/lesson10/Number29.java
@@ -1,6 +1,7 @@
 package main.java.lesson10;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -19,34 +20,17 @@ public class Number29 {
      */
     public static void main(String[] args) {
         final String text = "Дети постоянно хотят  смотреть мультики. Мультики, мультики... это ведь дети.";
-        final List<String> wordText = Arrays.asList
-                (text.toLowerCase().replaceAll("[^A-Za-zА-Яа-я_]", " ").split("\\s+"));
-        final Map<String, Long> wordToCount = getMap(wordText);
+        final Map<String, Long> wordToCount = getMap(text);
         wordToCount.forEach((k, v) -> System.out.println(k + " - " + v));
     }
 
     /**
      * Main solution : getMap
      */
-    private static Map<String, Long> getMap(final List<String> wordText) {
-        return wordText.stream()
-                .flatMap(Stream::of)
-                .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
-    }
-
-    /**
-     * Other solutions:  getMap1,  getMap2
-     */
-    private static Map<String, Long> getMap1(final List<String> wordText) {
-        Map<String, Long> newMap = new HashMap<>();
-        wordText.forEach(word -> newMap.put(word, newMap.containsKey(word) ? newMap.get(word) + 1 : 1));
+    private static Map<String, Long> getMap(final String text) {
+        Map<String, Long> newMap;
+        newMap = text.lines().flatMap(word -> Stream.of(word.toLowerCase().replaceAll("[^A-Za-zА-Яа-я_]", " ").split("\\s+")))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         return newMap;
-    }
-
-    private static Map<String, Long> getMap2(final List<String> wordText) {
-        Map<String, Long> newMap2 = new HashMap<>();
-        newMap2 = wordText.stream()
-                .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
-        return newMap2;
     }
 }
---
+++ src/main/java/lesson10/Number29OtherSolutions.java
@@ -0,0 +1,46 @@
+package main.java.lesson10;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * The type Number 29.
+ * Имеется текст. Следует составить для него частотный словарь
+ */
+public class Number29OtherSolutions {
+    /**
+     * The entry point of application.
+     * Regular expressions :
+     * "[^A-Za-zА-Яа-я_]" - not letters;
+     * "\\s+" - one or more spaces.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        final String text = "Дети постоянно хотят  смотреть мультики. Мультики, мультики... это ведь дети.";
+        final List<String> wordText = Arrays.asList
+                (text.toLowerCase().replaceAll("[^A-Za-zА-Яа-я_]", " ").split("\\s+"));
+        final Map<String, Long> wordToCount = getMap1(wordText);
+        wordToCount.forEach((k, v) -> System.out.println(k + " - " + v));
+    }
+
+
+    /**
+     * Other solutions:  getMap1
+     */
+    private static Map<String, Long> getMap1(final List<String> wordText) {
+        Map<String, Long> newMap1 = new HashMap<>();
+        wordText.forEach(word -> newMap1.put(word, newMap1.containsKey(word) ? newMap1.get(word) + 1 : 1));
+        return newMap1;
+    }
+
+    /**
+     * Other solutions:  getMap2
+     */
+    private static Map<String, Long> getMap2(final List<String> wordText) {
+        Map<String, Long> newMap2;
+        newMap2 = wordText.stream()
+                .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
+        return newMap2;
+    }
+}
