a9afe546f604915c91d8a19a489015f67fc4cb49
RozovaOlga <olinkakrasavitsa@gmail.com>
2020-03-18 19:54:12 +0200

    28

---
+++ src/main/java/lesson11/Number28.java
@@ -0,0 +1,40 @@
+package main.java.lesson11;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+public class Number28 {
+    public static void main(String[] args) {
+        final List<Integer> rating = randomRaring();
+        System.out.println(rating);
+        System.out.println(getMax(rating));
+    }
+
+    private static List<Integer> randomRaring() {
+        int i = 10;
+        final List<Integer> randomNumbers = new ArrayList<Integer>(i);
+        final Random rand = new Random();
+        while (i-- > 0) {
+            randomNumbers.add(rand.nextInt(10));
+        }
+        return randomNumbers;
+    }
+
+    private static Integer getMax(final List<Integer> rating) {
+        Iterator<Integer> iterator = rating.iterator();
+        Integer max = null;
+        while (iterator.hasNext()) {
+            if (max == null) {
+                max = iterator.next();
+            } else {
+                Integer a = iterator.next();
+                if (a.compareTo(max) > 0) {
+                    max = a;
+                }
+            }
+        }
+        return max;
+    }
+}
